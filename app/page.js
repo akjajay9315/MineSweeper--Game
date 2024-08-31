3640e0add12b34ade079fe67fed7119056bf14c1
Ajay kumar <akjajay9315@gmail.com>
2024-08-31 23:58:26 +0530

    button font

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import Game from '@/components/game/Game';
-import Restart from '@/components/buttons/Restart';
+import React, { useState, useEffect } from "react";
+import Game from "@/components/game/Game";
+import Restart from "@/components/buttons/Restart";
 
 export default function Home() {
   const [time, setTime] = useState(0);
@@ -13,7 +13,7 @@ export default function Home() {
 
     if (timerRunning) {
       intervalId = setInterval(() => {
-        setTime(prevTime => prevTime + 1);
+        setTime((prevTime) => prevTime + 1);
       }, 1000);
     } else {
       clearInterval(intervalId);
@@ -35,20 +35,26 @@ export default function Home() {
     const minutes = Math.floor((timeInSeconds % 3600) / 60);
     const seconds = timeInSeconds % 60;
 
-    return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+    return `${hours < 10 ? "0" + hours : hours}:${
+      minutes < 10 ? "0" + minutes : minutes
+    }:${seconds < 10 ? "0" + seconds : seconds}`;
   };
 
   return (
-    <div className="p-2 flex-col gap-4 flex items-center max-w-[800px] w-11/12">
-      <div className='flex w-full bg-white bg-opacity-5 font-medium text-xl backdrop-blur-sm rounded-lg py-2 px-2'>
-        <div className='bg-white bg-opacity-5 px-3 py-2 rounded-md'>MineSweeper</div>
-        <div className='flex-1 text-center px-3 py-2'>{formatTime(time)}</div>
-        <div className='flex'>
+    <div className="p-2 flex-col gap-4 flex items-center max-w-[800px] w-11/12 mx-auto">
+      <div className="flex w-full bg-white bg-opacity-10 font-medium text-lg md:text-xl backdrop-blur-sm rounded-lg py-2 px-4">
+        <div className="bg-white bg-opacity-10 px-3 py-2 rounded-md text-sm md:text-base">
+          MineSweeper
+        </div>
+        <div className="flex-1 text-center px-3 py-1 text-sm md:text-base">
+          {formatTime(time)}
+        </div>
+        <div className="flex">
           <Restart />
         </div>
       </div>
-      <div className='flex w-full'>
-        <Game time={time} startTimer = {startTimer} stopTimer = {stopTimer}/>
+      <div className="flex w-full">
+        <Game time={time} startTimer={startTimer} stopTimer={stopTimer} />
       </div>
     </div>
   );
--- components/buttons/Restart.jsx
+++ components/buttons/Restart.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 
 export default function Restart() {
   return (
-    <div onClick={()=> window.location.reload()} className='bg-white cursor-pointer bg-opacity-5 px-3 py-2 rounded-md hover:bg-opacity-10'>Restart</div>
-  )
+    <div
+      onClick={() => window.location.reload()}
+      className="bg-white cursor-pointer bg-opacity-5 px-3 py-2 rounded-md hover:bg-opacity-10 text-sm">
+      Restart
+    </div>
+  );
 }
